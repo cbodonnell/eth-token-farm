5c4e7fd7f10ee95dae2f2d679ee691fa0782cb29
cbodonnell <cheebzmusic@gmail.com>
2020-12-19 20:52:58 -0500

    clean up

--- src/components/App.js
+++ src/components/App.js
@@ -20,7 +20,7 @@ class App extends Component {
             <main role="main" className="col-lg-12 ml-auto mr-auto" style={{ maxWidth: '600px' }}>
               <div className="content mr-auto ml-auto">
                 <a
-                  href="http://www.dappuniversity.com/bootcamp"
+                  href="http://www.studio10b.nyc"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
